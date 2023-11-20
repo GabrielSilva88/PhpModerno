069955e6f74a545157d99dee9f2b29d43ad17fba
GabrielSilva88 <gabrielmg.silva@outlook.com>
2023-11-20 19:30:55 -0300

    Corrigir variavel "nacs" to "nasc";

--- desafios/d010/index.php
+++ desafios/d010/index.php
@@ -11,15 +11,16 @@
 <body>
     <?php
     $atual = date("Y");
-    $nasc = $_GET['nasc'] ?? 2000;
+    $nasc = $_GET['nasc'] ?? '2000';
     $ano = $_GET['ano'] ?? $atual;
     ?>
     <main>
+        <!-- -->
         <h1>Calculando a sua idade</h1>
         <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
             <label for="nasc">Em que ano você nasceu?</label>
-            <input type="number" name="nacs" id="nasc" min="1900" value="<?= $nasc ?>">
-            <label for="ano">Quer saber sua idade em que ano?(atualmente estamos <strong><?= $atual ?></strong>)</label>
+            <input type="number" name="nasc" id="nasc" min="1900" value="<?= $nasc ?>">
+            <label for="ano">Quer saber sua idade em que ano? (atualmente estamos <strong><?= $atual ?></strong>)</label>
             <input type="number" name="ano" id="ano" min="1900" value="<?= $ano ?>">
             <input type="submit" value="Qual será sua idade?">
         </form>
